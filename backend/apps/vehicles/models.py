9b7975dcd2b1d87eedb62ef2b7e92d68fa2a66c2
shahdhesham5 <shahd.hesham1605@gmail.com>
2025-06-18 13:22:58 +0300

    Drivers CRUD

--- backend/apps/vehicles/models.py
+++ backend/apps/vehicles/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Driver(models.Model):
+    STATUS_CHOICES = [
+        ("active", "Active"),
+        ("inactive", "Inactive"),
+    ]
+    name = models.CharField(max_length=250)
+    license = models.CharField(max_length=14)
+    license_category = models.CharField(max_length=100, null=True)
+    license_expiry = models.DateField(null=True)
+    notes = models.CharField(max_length=250, null=True, blank=True)
+    status = models.CharField(
+        max_length=8, choices=STATUS_CHOICES, default="active", null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.name
--- backend/apps/vehicles/views.py
+++ backend/apps/vehicles/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Driver
+from .serializers import DriverSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework import permissions
 
-# Create your views here.
+
+class DriverListCreateAPIView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request):
+        drivers = Driver.objects.all()
+        serializer = DriverSerializer(drivers, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = DriverSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DriverDetailAPIView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request, pk):
+        driver = get_object_or_404(Driver, pk=pk)
+        serializer = DriverSerializer(driver)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        driver = get_object_or_404(Driver, pk=pk)
+        serializer = DriverSerializer(driver, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        driver = get_object_or_404(Driver, pk=pk)
+        driver.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)

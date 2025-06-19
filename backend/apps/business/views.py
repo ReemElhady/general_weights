45d57be5cea46570dc9a2acd9b6162bc9dba4a33
Mahmoud Abozahra <mahmoudabozahra151@gmail.com>
2025-06-19 11:55:57 +0300

    add scale in business app

--- backend/apps/business/views.py
+++ backend/apps/business/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Scale
+from .serializers import ScaleSerializer
+
 
 # Create your views here.
+
+
+class viewsets_scale(viewsets.ModelViewSet):
+    queryset = Scale.objects.all()
+    serializer_class = ScaleSerializer
+    
